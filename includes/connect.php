43becda7a7b25c5afa912b47d6f70fc75a0fd39e
Maxwell Karp <maxwell.karp@yahoo.com>
2015-04-14 10:58:17 -0700

    video 7 coded and commented

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 	//connects code to localhost
-	$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 	//if $mysqli fails, then it dies and gives message
 	if ($mysqli->connect_error){
 		die('Connect Error (' . $mysqli->connect_errno . ')' . $mysqli->connect_error);
